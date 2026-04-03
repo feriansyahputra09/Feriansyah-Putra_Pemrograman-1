ceacd8e6ce1bcf3654c35ec4262720a260f17e8d
Feriansyah Putra <feriansyahputra689@gmail.com>
2026-04-03 19:47:10 +0700

    Add files via upload

---
+++ P2/latihan1/Latihan1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Latihan1 {
+    public static void main(String[]args) {
+        Scanner input = new Scanner(System.in);
+        
+       String nama, hobi, alamat;
+       int umur;
+       
+       System.out.print("Masukkan nama: ");
+       nama = input.nextLine();
+       
+       System.out.print("Masukkan umur: ");
+       umur = input.nextInt();
+       input.nextLine();
+       
+       System.out.print("Masukkan hobi: ");
+       hobi = input.nextLine();
+       
+       System.out.print("Masukkan alamat: ");
+       alamat = input.nextLine();
+       
+       System.out.println("\n=== Data Diri Lengkap ===");
+       System.out.println("nama     :" + nama);
+       System.out.println("umur     :" + umur);
+       System.out.println("hobi     :" + hobi);
+       System.out.println("alamat   :" + alamat);
+    }
+}
