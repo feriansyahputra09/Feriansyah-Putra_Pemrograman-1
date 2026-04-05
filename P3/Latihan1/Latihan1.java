46a44bf42de1898874997b7f2052f07bebf7a9ba
Feriansyah Putra <feriansyahputra689@gmail.com>
2026-04-05 16:27:22 +0700

    Add files via upload

---
+++ P3/Latihan1/Latihan1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Latihan1 {
+    public static void main(String [] args ) {
+     
+        
+        String Nama = "Feriansyah Putra";
+        int Umur = 20;
+        double IPK = 3.45;
+        char Grade = 'A';
+        boolean Lulus = true;
+        
+         System.out.println("==============================");
+         System.out.println("       DATA MAHASISWA ");
+         System.out.println("==============================");
+         System.out.println("Nama        : " + Nama);
+         System.out.println("Umur        : " + Umur + " Tahun ");
+         System.out.println("IPK        : " + IPK);
+         System.out.println("Grade        : " + Grade);
+         System.out.println("Status Lulus        : " + Lulus);
+         System.out.println("=================================");
+    }
+}
